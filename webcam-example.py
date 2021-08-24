2674b31f8c613555c0823b62c0b0a037aa343ad3
Huw <vfxgames2014@gmail.com>
2021-08-24 15:53:40 +0100

    Slow with larger datasets

--- webcam-example.py
+++ webcam-example.py
@@ -1,20 +1,41 @@
 import cv2
+import time
 import detector
 from deepface import DeepFace
+
+
+# Config deepface
+model = 'Facenet'
+detector_type = 'dlib'
+distance_metric = 'euclidean'# 'euclidean_l2'
 
 identity = ""
 recognize = 1
 cap = cv2.VideoCapture(0)
 
+# font which we will be using to display FPS
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# used to record the time when we processed last frame
+prev_frame_time = 0
+
+# used to record the time at which we processed current frame
+new_frame_time = 0
+
 # Initialize the detector with the chosen model and resize value
-det = detector.FrameDetector('mtcnn', 0.5)
+det = detector.FrameDetector(detector_type, 0.2)
 
 while True:
     # Read frame from cv2 source
     ret, img = cap.read()
 
+    # Convert to greyscale
+
+
     # Detect faces in frame
     img, detections = det.detect(img)
+
+
 
     # If faces are detected and recognize is enabled, recognize the face
     if len(detections) > 0 and recognize == 1 and ret:
@@ -23,31 +44,53 @@ while True:
         for idx, detection in enumerate(detections):
 
             # Get face from frame
-            crop_img = img[detection[1]:detection[1]+detection[3], detection[0]:detection[0]+detection[2]]
-
+            crop_img = img[detection[1]:detection[1] +
+                           detection[3], detection[0]:detection[0]+detection[2]]
 
             # Write face to file
             cv2.imwrite(str(idx) + '.jpg', crop_img)
 
             # Recognize the face
             recognition = DeepFace.find(
-                img_path = str(idx) + '.jpg', 
-                db_path = "./dataset", 
-                model_name = 'Facenet', 
-                enforce_detection = False,
-                distance_metric = 'euclidean',
-                detector_backend = 'mtcnn')
+                img_path=str(idx) + '.jpg',
+                db_path="./dataset",
+                model_name=model,
+                enforce_detection=False,
+                distance_metric=distance_metric,
+                detector_backend=detector_type)
 
             if recognition.shape[0] > 0:
                 identity = recognition.iloc[0].identity
                 # identity = identity.split('/')[5]
-                cv2.putText(img, identity, (detection[0], detection[1]), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 255, 0), 2, cv2.LINE_AA)
-    
+                cv2.putText(img, identity, (detection[0], detection[1]),
+                            cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 255, 0), 2, cv2.LINE_AA)
+
             else:
                 identity = "Unknown"
-        
+
+
+           
+
     else:
         identity = ""
+
+    new_frame_time = time.time()
+            
+    # fps will be number of frame processed in given time frame
+    # since their will be most of time error of 0.001 second
+    # we will be subtracting it to get more accurate result
+    fps = 1/(new_frame_time-prev_frame_time)
+    prev_frame_time = new_frame_time
+
+    # converting the fps into integer
+    fps = int(fps)
+
+    # converting the fps to string so that we can display it on frame
+    # by using putText function
+    fps = str(fps)
+
+    # putting the FPS count on the frame
+    cv2.putText(img, fps, (7, 70), font, .5, (100, 255, 0), 1, cv2.LINE_AA)
 
     # Add text to frame and show
     cv2.imshow('Input', img)
